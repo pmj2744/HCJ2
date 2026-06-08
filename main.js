8e419e5d8706c3deea7ff6fc45d10d2036640e2f
pmj2744 <pmj2744@gmail.com>
2026-06-08 15:44:09 +0900

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+document.getElementtById('time').innerHTML = new Date().toLocaleString()
+
+function pink() {
+  document.body.style.backgroundColor = 'pink'
+}
+
+function lightblue() {
+  document.body.style.backgroundColor = 'lightblue'
+}
+
+function yellow() {
+  document.body.style.backgroundColor = 'yellow'
+}
+
+function reset() {
+  document.body.style.backgroundColor = 'linen'
+}
+
+function showhtml() {
+  document.getElementById('fig').src = 'img/HTML5_logo.png'
+  document.getElementById('desc').innerHTML =
+    'Html은 웹문서를 작성하는데 사용하는 마크업 언어이다.'
+}
+
+function showcss() {
+  document.getElementById('fig').src = 'img/CSS3_logo.png'
+  document.getElementById('desc').innerHTML =
+    'css는 hTML 문서의 스타일을 지정하기 위한 언어이다.'
+}
+
+function showjs() {
+  document.getElementById('fig').src = 'img/Unofficial_JavaScript_logo.png'
+  document.getElementById('desc').innerHTML =
+    'javascript는 웹을 위한 프로그래밍 언어이며 웹문서에서 동작을 실행할 수 있게 한다.'
+}
+
+function hide() {
+  document.getElementById('fig').src = ''
+  document.getElementById('desc').innerHTML = ''
+}
